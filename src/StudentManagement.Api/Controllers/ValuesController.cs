f6b83d1a2ccbaae6d7773123bf62cb9337ed48f1
Ahasan <shobuz.cse03@gmail.com>
2020-11-09 11:34:36 +0600

    init update

---
+++ src/StudentManagement.Api/Controllers/ValuesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentManagement.DataAccess;
+
+namespace StudentManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly ValuesRepository _repository;
+
+        public ValuesController(ValuesRepository repository)
+        {
+            _repository = repository??throw new ArgumentNullException(nameof(repository));
+        }
+        [HttpGet]
+        public async Task<List<Value>> Get()
+        {
+            return await _repository.GetAll();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Value>> Get(int id)
+        {
+            var response = await _repository.GetById(id);
+            if (response == null) { return NotFound(); }
+            return response;
+        }
+        [HttpPost]
+        public async Task Post([FromBody] Value value) {
+            await _repository.Insert(value);
+        }
+        [HttpDelete("{id}")]
+        public async Task Delete(int id)
+        {
+            await _repository.DeleteById(id);
+        }
+
+
+    }
+}
---
+++ src/StudentManagement.DataAccess/Value.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace StudentManagement.DataAccess
+{
+    public class Value
+    {
+        public int Id { get; set; }
+        public int Value1 { get; set; }
+        public string Value2 { get; set; }
+    }
+}
